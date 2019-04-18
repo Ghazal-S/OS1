bb9f3da05a642e3bf26d48ac724cfd5372b33506
Ghazal <sadeghian.ghazal77@gmail.com>
2019-04-18 22:50:30 +0430

    writing client and server in python for Q4

---
+++ Q4Server.py
@@ -0,0 +1,57 @@
+import select, socket, sys, Queue,re
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setblocking(0)
+server.bind(('localhost', 50000))
+server.listen(5)
+inputs = [server]
+outputs = []
+message_queues = {}
+groups = {}
+
+while inputs:
+    readable, writable, exceptional = select.select(
+        inputs, outputs, inputs)
+    for s in readable:
+        if s is server:
+            connection, client_address = s.accept()
+            connection.setblocking(0)
+            inputs.append(connection)
+            message_queues[connection] = Queue.Queue()
+        else:
+            data = s.recv(1024)
+            if data:
+                message_queues[s].put(data)
+                if data.split("[")[0] == "join" :
+                    groups[data.split("[")[1]].append(connection)
+                elif data.split("[")[0] == "send" :
+                    for client in groups[groups[data.split("[")[1]]]:
+                        client.send(groups[data.split("[")[2]])
+                elif data.split("[")[0] == "leave" :
+                    groups[data.split("[")[1]].remove(connection)
+                elif data.split("[")[0] == "quit" :
+                    s.close()
+
+                        
+                if s not in outputs:
+                    outputs.append(s)
+            else:
+                if s in outputs:
+                    outputs.remove(s)
+                inputs.remove(s)
+                s.close()
+                del message_queues[s]
+
+    for s in writable:
+        try:
+            next_msg = message_queues[s].get_nowait()
+        except Queue.Empty:
+            outputs.remove(s)
+        else:
+            s.send(next_msg)
+
+    for s in exceptional:
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+        del message_queues[s]
